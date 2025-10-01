060df50d1910ecd18ef9ad3e4740870762a3f1d7
lordShandilya <vshandilya220@gmail.com>
2025-10-01 10:31:26 +0530

    feat(ocr): integrate text recognition with ML kit

--- cfo/utils/ocr.tsx
+++ cfo/utils/ocr.tsx
@@ -3,7 +3,7 @@ import TextRecognition from "@react-native-ml-kit/text-recognition";
 
 export const captureTransaction = async (uri: string) => {
     try {
-        const resultFromCapture = await TextRecognition.recognize(uri   );
+        const resultFromCapture = await TextRecognition.recognize(uri);
         console.log(resultFromCapture.text);
     } catch (err) {
         console.error("Error while capturing the text", err);
